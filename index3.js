84665e5053c4a5699594492dd85c18a96c4441a2
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-05-30 17:56:37 +0200

    Fix the line response.setHeader in index3.js and change contents in index.html

--- index3.js
+++ index3.js
@@ -1,26 +1,24 @@
 var fs = require('fs');
-var buffer = require('buffer');
-var path = require('path');
-
 var http = require('http');
-
 var server = http.createServer();
-var image =
+
 
 server.on('request', function(request, response) {
-  response.setHeader("Content-Type", "text/html; charset=utf-8");
   if (request.method === 'GET' && request.url === '/index3') {
+    // line 13 -use it only if you enter the index3
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
     fs.readFile('./index.html', 'utf-8', function(err, data) {
     response.write(data);
-    response.write("zalaczylo sie");
+    response.write('here is your page');
     response.end();
     })
   } else {
     response.statusCode = 404;
-    fs.readFile('./firstframe.jpg', function(err, data) {
-    // fs.readFile('./error404.gif', function(err, data) {
+    // line 22--> open picture & line 23-->open .gif
+    // fs.readFile('./firstframe.jpg', function(err, data) {
+    fs.readFile('./error404.gif', function(err, data) {
       response.write(data);
-      response.write('nie udalo sie-zła ściezka');
+      response.write('bad path');
       response.end();
     })
   }
